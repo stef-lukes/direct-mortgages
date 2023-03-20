e5188fe30c72025acaa6c8fa4650ecdad9976b8f
stef-lukes <stefanlukes@gmail.com>
2023-03-20 13:56:15 +0000

    Adds full JS functionality for Mortgage Calculator. Further styling needed

--- script.js
+++ script.js
@@ -1,87 +1,101 @@
-// Get the input elements
-var propertyValue = document.getElementById("propertyValue");
-var availableDeposit = document.getElementById("availableDeposit");
-var interestRate = document.getElementById("interestRate");
-var loanTerm = document.getElementById("loanTerm");
+// Get DOM elements
+const propertyValue = document.getElementById("propertyValue");
+const interestRate = document.getElementById("interestRate");
+const loanTerm = document.getElementById("loanTerm");
+const availableDeposit = document.getElementById("availableDeposit");
+const mortgagePaymentOutput = document.getElementById("result");
+const totalInterestOutput = document.getElementById("totalInterestPayable");
+const capitalRepayment = document.getElementById("capitalRepayment");
+const interestOnly = document.getElementById("interestOnly");
 
-// Get the output elements
-var availableDepositOutput = document.getElementById("availableDepositOutput");
-var interestRateOutput = document.getElementById("interestRateOutput");
-var loanTermOutput = document.getElementById("loanTermOutput");
-var resultElement = document.getElementById("result");
-
-// Set the default property value and calculate the initial available deposit
+// Set default property value
 propertyValue.value = 200000;
-calculateAvailableDeposit();
 
-// Add event listeners to update the outputs as the sliders are adjusted
+// Set default available deposit value based on default property value
+availableDeposit.value = 10;
+const initialDepositAmount =
+  propertyValue.value * (availableDeposit.value / 100);
+document.getElementById(
+  "depositAmountOutput"
+).textContent = `£${initialDepositAmount.toFixed(2)}`;
+
+// Function to calculate the mortgage payment and total interest payable
+function calculateMortgagePayment() {
+  const principle = propertyValue.value * (1 - availableDeposit.value / 100);
+  const monthlyRate = interestRate.value / 100 / 12;
+  const totalPayments = loanTerm.value * 12;
+
+  let payment;
+  let totalInterest;
+
+  if (capitalRepayment.checked) {
+    const numerator =
+      principle * monthlyRate * (1 + monthlyRate) ** totalPayments;
+    const denominator = (1 + monthlyRate) ** totalPayments - 1;
+    payment = numerator / denominator;
+
+    totalInterest = payment * totalPayments - principle;
+  } else if (interestOnly.checked) {
+    payment = principle * monthlyRate;
+
+    totalInterest = principle * (interestRate.value / 100) * loanTerm.value;
+  }
+
+  mortgagePaymentOutput.textContent = `Your monthly mortgage payment is: £${payment.toFixed(
+    2
+  )}`;
+  totalInterestOutput.textContent = `Total Interest Payable: £${totalInterest.toFixed(
+    2
+  )}`;
+}
+
+// Add event listener for the mortgage type radio buttons
+document
+  .querySelectorAll("input[type='radio']")
+  .forEach(function (radioButton) {
+    radioButton.addEventListener("change", function () {
+      // Update the mortgage payment output value and total interest payable based on the new mortgage type
+      calculateMortgagePayment();
+    });
+  });
+
+// Add event listener for the property value input
 propertyValue.addEventListener("input", function () {
-  calculateAvailableDeposit();
+  // Update the deposit amount output value based on the new property value and available deposit percentage
+  const depositAmount = propertyValue.value * (availableDeposit.value / 100);
+  document.getElementById(
+    "depositAmountOutput"
+  ).textContent = `£${depositAmount.toFixed(2)}`;
+
+  // Update the mortgage payment output value and total interest payable based on the new property value
   calculateMortgagePayment();
 });
 
-availableDeposit.addEventListener("input", function () {
-  calculateMortgagePayment();
-  updateAvailableDepositOutput();
+// Add event listener for any of the slider inputs
+document.querySelectorAll("input[type='range']").forEach(function (slider) {
+  slider.addEventListener("input", function () {
+    // Update the deposit amount output value
+    const depositAmount = propertyValue.value * (availableDeposit.value / 100);
+    document.getElementById(
+      "depositAmountOutput"
+    ).textContent = `£${depositAmount.toFixed(2)}`;
+
+    // Update the mortgage payment output value and total interest payable
+    calculateMortgagePayment();
+  });
 });
 
+// Add event listeners for the interest rate and loan term sliders
 interestRate.addEventListener("input", function () {
-  updateInterestRateOutput();
+  document.getElementById(
+    "interestRateOutput"
+  ).textContent = `${interestRate.value}%`;
   calculateMortgagePayment();
 });
 
 loanTerm.addEventListener("input", function () {
-  updateLoanTermOutput();
+  document.getElementById(
+    "loanTermOutput"
+  ).textContent = `${loanTerm.value} years`;
   calculateMortgagePayment();
 });
-
-// Function to calculate the available deposit
-function calculateAvailableDeposit() {
-  // Calculate available deposit as percentage of property value
-  const percentage = Math.min(availableDeposit.value, 80);
-  const deposit = (propertyValue.value * percentage) / 100;
-
-  // Convert available deposit to dollar value for display
-  availableDepositOutput.value = `£${deposit.toLocaleString()}`;
-}
-
-// Event listener for property value input
-propertyValue.addEventListener("input", calculateAvailableDeposit);
-
-// Event listener for available deposit input
-availableDeposit.addEventListener("input", () => {
-  updateAvailableDepositOutput();
-});
-
-// Function to update available deposit output
-function updateAvailableDepositOutput() {
-  const percentage = Math.min(availableDeposit.value, 80);
-  availableDepositOutput.value = `£${(
-    (propertyValue.value * percentage) /
-    100
-  ).toLocaleString()}`;
-}
-
-// Function to update the interest rate output
-function updateInterestRateOutput() {
-  interestRateOutput.innerHTML = interestRate.value + "%";
-}
-
-// Function to update the loan term output
-function updateLoanTermOutput() {
-  loanTermOutput.innerHTML = loanTerm.value + " years";
-}
-
-// Function to calculate the mortgage payment
-function calculateMortgagePayment() {
-  var principal = propertyValue.value - availableDeposit.value;
-  var monthlyInterestRate = interestRate.value / 1200;
-  var numberOfPayments = loanTerm.value * 12;
-  var mortgagePayment =
-    (principal *
-      monthlyInterestRate *
-      Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
-    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-  resultElement.innerHTML =
-    "Your monthly mortgage payment is: £" + mortgagePayment.toFixed(2);
-}
